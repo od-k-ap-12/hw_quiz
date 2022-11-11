ba0f45ac0e6bcd1db7ca8bc8470b3058a29ce7e8
Oleksandra <predictormagnolia@gmail.com>
2022-11-11 07:01:36 +0200

    Add project files.

---
+++ WindowsProject7/WindowsProject7.cpp
@@ -0,0 +1,147 @@
+﻿// WindowsProject7.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "WindowsProject7.h"
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+HWND hCheckBox1,hCheckBox2,hEdit;
+
+
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+}
+
+
+
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    //wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT7));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WINDOWSPROJECT7);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance;
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wp, LPARAM lp)
+{
+    TCHAR str[100];
+    int RightAnswers = 0;
+    int MaxRightAnswers = 14;
+    switch (message)
+    {
+    case WM_CLOSE:
+        EndDialog(hWnd, 0);
+        return TRUE;
+
+    case  WM_INITDIALOG:
+
+        return TRUE;
+
+    case WM_COMMAND:{
+        LRESULT result1 = SendDlgItemMessage(hWnd, IDC_RADIO1, BM_GETCHECK, 0, 0);
+        LRESULT result2;
+        if (result1 == BST_CHECKED)
+            RightAnswers++;
+        result1 = SendDlgItemMessage(hWnd, IDC_RADIO4, BM_GETCHECK, 0, 0);
+        if (result1 == BST_CHECKED)
+            RightAnswers++;
+        result1 = SendDlgItemMessage(hWnd, IDC_RADIO7, BM_GETCHECK, 0, 0);
+        if (result1 == BST_CHECKED)
+            RightAnswers++;
+        result1 = SendDlgItemMessage(hWnd, IDC_RADIO10, BM_GETCHECK, 0, 0);
+        if (result1 == BST_CHECKED)
+            RightAnswers++;
+        result1 = SendDlgItemMessage(hWnd, IDC_RADIO13, BM_GETCHECK, 0, 0);
+        if (result1 == BST_CHECKED)
+            RightAnswers++;
+        result1= SendDlgItemMessage(hWnd, IDC_RADIO16, BM_GETCHECK, 0, 0);
+        if (result1== BST_CHECKED)
+            RightAnswers++;
+        result1 = SendDlgItemMessage(hWnd, IDC_RADIO19, BM_GETCHECK, 0, 0);
+        if (result1 == BST_CHECKED)
+            RightAnswers++;
+        result1 = SendDlgItemMessage(hWnd, IDC_RADIO22, BM_GETCHECK, 0, 0);
+        if (result1 == BST_CHECKED)
+            RightAnswers++;
+        hCheckBox1 = GetDlgItem(hWnd, IDC_CHECK1);
+        hCheckBox2 = GetDlgItem(hWnd, IDC_CHECK2);
+        result1 = SendMessage(hCheckBox1, BM_GETCHECK, 0, 0);
+        result2 = SendMessage(hCheckBox2, BM_GETCHECK, 0, 0);
+        if (result1 ==BST_CHECKED&&result2==BST_CHECKED) {
+            RightAnswers++;
+        }
+        hCheckBox1 = GetDlgItem(hWnd, IDC_CHECK4);
+        hCheckBox2 = GetDlgItem(hWnd, IDC_CHECK5);
+        result1 = SendMessage(hCheckBox1, BM_GETCHECK, 0, 0);
+        result2 = SendMessage(hCheckBox2, BM_GETCHECK, 0, 0);
+        if (result1 == BST_CHECKED && result2 == BST_CHECKED) {
+            RightAnswers++;
+        }
+        hEdit = GetDlgItem(hWnd, IDC_EDIT2);
+        TCHAR Edit1[10],Edit2[10];
+        GetWindowText(hEdit, Edit1, 10);
+        if (_tcscmp(Edit1,TEXT("Лампочка"))==0)
+            RightAnswers++;
+        hEdit = GetDlgItem(hWnd, IDC_EDIT1);
+        GetWindowText(hEdit, Edit2, 10);
+        if (_tcscmp(Edit1, TEXT("15")) == 0)
+            RightAnswers++;
+        int Result= (RightAnswers * 100) / MaxRightAnswers;
+        if (LOWORD(wp) == IDOK) {
+            _stprintf_s(str, TEXT("Правильных ответов: %d, out of 100 percents "), Result);
+            MessageBox(hWnd, str, TEXT("Ты лох"), MB_OK | MB_ICONINFORMATION);
+        }
+        }
+        return TRUE;
+    }
+    return FALSE;
+}
---
+++ WindowsProject7/resource.h
@@ -0,0 +1,67 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WindowsProject7.rc
+//
+#define IDC_MYICON                      2
+#define IDD_WINDOWSPROJECT7_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINDOWSPROJECT7             107
+#define IDI_SMALL                       108
+#define IDC_WINDOWSPROJECT7             109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDB_BITMAP2                     131
+#define IDB_BITMAP3                     132
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_RADIO1                      1003
+#define IDC_RADIO2                      1004
+#define IDC_RADIO3                      1005
+#define IDC_CHECK1                      1006
+#define IDC_CHECK2                      1007
+#define IDC_CHECK3                      1008
+#define IDC_CHECK4                      1009
+#define IDC_CHECK5                      1010
+#define IDC_CHECK6                      1011
+#define IDC_RADIO4                      1012
+#define IDC_RADIO5                      1013
+#define IDC_RADIO6                      1014
+#define IDC_RADIO7                      1015
+#define IDC_RADIO8                      1016
+#define IDC_RADIO9                      1017
+#define IDC_RADIO10                     1018
+#define IDC_RADIO11                     1019
+#define IDC_RADIO12                     1020
+#define IDC_RADIO13                     1021
+#define IDC_RADIO14                     1022
+#define IDC_RADIO15                     1023
+#define IDC_RADIO16                     1024
+#define IDC_RADIO17                     1025
+#define IDC_RADIO18                     1026
+#define IDC_RADIO19                     1027
+#define IDC_RADIO20                     1028
+#define IDC_RADIO21                     1029
+#define IDC_RADIO22                     1030
+#define IDC_RADIO23                     1031
+#define IDC_RADIO24                     1032
+#define IDC_RADIO25                     1033
+#define IDC_EDIT3                       1035
+#define IDC_SPIN2                       1036
+#define IDC_EDIT4                       1037
+#define IDC_SPIN3                       1038
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1039
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
