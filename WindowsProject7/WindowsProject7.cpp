991cddb10b2ad8b67c73adaaf3510dc5805d8127
Oleksandra <predictormagnolia@gmail.com>
2022-11-14 00:22:29 +0200

    exit menu

--- WindowsProject7/WindowsProject7.cpp
+++ WindowsProject7/WindowsProject7.cpp
@@ -3,10 +3,6 @@
 
 #include "framework.h"
 #include "Commctrl.h"
-#include <windows.h>
-#include <windowsX.h>
-#include <tchar.h>
-#include "resource.h"
 #pragma comment(lib,"comctl32")
 #include "WindowsProject7.h"
 
